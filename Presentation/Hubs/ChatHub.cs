9cb6dc6bb57f69df11a2c7ca319c21c4a857dffc
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-07-29 15:55:40 +0300

    Refactoring

---
+++ Presentation/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Presentation.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private Dictionary<string,string> keyValuePairs = new Dictionary<string,string>();
+        public ChatHub() 
+        {
+
+        }
+        public async Task sendmessage(string message)
+        {
+            await Clients.All.SendAsync("recievemessage", message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
