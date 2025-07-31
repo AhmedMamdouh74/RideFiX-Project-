b603ffb01fb77d92fb8f63f23b7570a0605ed025
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-07-31 11:55:51 +0300

    Nothing

--- Presentation/Controllers/ChatController.cs
+++ Presentation/Controllers/ChatController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using SharedData.DTOs;
@@ -11,6 +12,7 @@ using SharedData.Wrapper;
 
 namespace Presentation.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ChatController : ControllerBase
@@ -21,17 +23,11 @@ namespace Presentation.Controllers
             serviceManager = _serviceManager;
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> GetAllChats(int Id)
-        //{
-
-        //    return Ok(ApiResponse<RequestBreifDTO>.SuccessResponse(Request, "Has a Request"));
-        //}
         [HttpGet("GetAllChats")]
         public IActionResult GetAllChhat()
         {
             var chats = serviceManager.ChatService.GetAllChatsAsync();
-            if (chats == null || !chats.Result.Any())
+            if (chats == null || chats.Result == null)
             {
                 return NotFound();
             }
