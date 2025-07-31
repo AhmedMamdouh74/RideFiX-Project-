bdc2d84cc62a9d3ab0e2d2bbcbf291c73b0db26f
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-07-31 11:02:35 +0300

    RequestWatchDog

--- ServiceAbstraction/IServiceManager.cs
+++ ServiceAbstraction/IServiceManager.cs
@@ -19,6 +19,7 @@ namespace ServiceAbstraction
 
         ICarOwnerService carOwnerService { get; }
         IUserProfileService userProfileService { get; }
+        IUserConnectionIdService userConnectionIdService { get; }
 
 
     }
---
+++ Services/CoreServices/UserConnectionIdService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Contracts.ReposatoriesContract;
+using Domain.Contracts.SpecificationContracts;
+using Domain.Entities.CoreEntites.EmergencyEntities;
+using ServiceAbstraction.CoreServicesAbstractions;
+using SharedData.DTOs.ConnectionDtos;
+
+namespace Service.CoreServices
+{
+    public class UserConnectionIdService : IUserConnectionIdService
+    {
+        IUnitOfWork UnitOfWork { get; set; }
+        private readonly IMapper _mapper;
+
+        public UserConnectionIdService(IUnitOfWork UnitOfWork, IMapper mapper)
+        {
+            this.UnitOfWork = UnitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(UserConnectionIdDto dto)
+        {
+            var entity = _mapper.Map<UserConnectionIds>(dto);
+            await UnitOfWork.ConnectionIdsRepository.AddAsync(entity);
+            await UnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<UserConnectionIdDto?> GetByIdsAsync(UserConnectionIdDto keyDto)
+        {
+            var entity = await UnitOfWork.ConnectionIdsRepository.GetByIdsAsync(keyDto.ConnectionId, keyDto.ApplicationUserId);
+            return entity is null ? null : _mapper.Map<UserConnectionIdDto>(entity);
+        }
+
+        public async Task<UserConnectionIdDto?> GetBySpecIdAsync(IUserConnectionIdsSpecification spec)
+        {
+            var entity = await UnitOfWork.ConnectionIdsRepository.GetBySpecIdAsync(spec);
+            return entity is null ? null : _mapper.Map<UserConnectionIdDto>(entity);
+        }
+
+        public async Task DeleteAsync(UserConnectionIdDto keyDto)
+        {
+            await UnitOfWork.ConnectionIdsRepository.DeleteAsync(keyDto.ConnectionId, keyDto.ApplicationUserId);
+            await UnitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<UserConnectionIdDto>> GetAllAsync()
+        {
+            var list = await UnitOfWork.ConnectionIdsRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<UserConnectionIdDto>>(list);
+        }
+
+        public async Task<IEnumerable<UserConnectionIdDto>> GetAllAsync(IUserConnectionIdsSpecification spec)
+        {
+            var list = await UnitOfWork.ConnectionIdsRepository.GetAllAsync(spec);
+            return _mapper.Map<IEnumerable<UserConnectionIdDto>>(list);
+        }
+
+        public async Task UpdateAsync(UserConnectionIdDto dto)
+        {
+            var entity = _mapper.Map<UserConnectionIds>(dto);
+            await UnitOfWork.ConnectionIdsRepository.UpdateAsync(entity);
+            await UnitOfWork.SaveChangesAsync();
+        }
+    }
+
+}
