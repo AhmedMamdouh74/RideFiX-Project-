55103897c50d8f454314d7d41fae7f98818731d5
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-08-05 20:29:19 +0300

    add spec to get the chat if found

--- Services/Specification_Implementation/ChatSessionsSpecifications/ChatSessionBetweenParticipantsSpec.cs
+++ Services/Specification_Implementation/ChatSessionsSpecifications/ChatSessionBetweenParticipantsSpec.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Entities.CoreEntites.EmergencyEntities;
+using Services.Specification_Implementation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace Service.Specification_Implementation.ChatSessionsSpecifications
 {
-    internal class ChatSessionBetweenParticipantsSpec
+    public class ChatSessionBetweenParticipantsSpec : Specification<ChatSession, int>
     {
+        public ChatSessionBetweenParticipantsSpec(int carOwnerId, int technicianId)
+            : base(cs => cs.CarOwnerId == carOwnerId && cs.TechnicianId == technicianId)
+        {
+        }
     }
 }
