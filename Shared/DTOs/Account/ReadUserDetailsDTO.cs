0e7abbea47edd2896b5d122127f8366ad0bdfd80
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-08-11 18:39:30 +0300

    add Rate prob for tech profile

--- Shared/DTOs/Account/ReadUserDetailsDTO.cs
+++ Shared/DTOs/Account/ReadUserDetailsDTO.cs
@@ -20,6 +20,7 @@ namespace SharedData.DTOs.Account
         public string? Address { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Description { get; set; }
+        public int Rate { get; set; }
        
 
     }
