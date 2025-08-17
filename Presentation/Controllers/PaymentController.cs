dcf08f68e439d2b6552f121102ecba02523023b1
Ahmed ElKholy <a.elkholy2711@gmail.com>
2025-08-17 17:34:47 +0300

    Payment Controller

--- Presentation/Controllers/PaymentController.cs
+++ Presentation/Controllers/PaymentController.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using SharedData.DTOs.Payment;
 
 namespace Presentation.Controllers
 {
-    internal class PaymentController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
     {
+        private readonly IServiceManager _serviceManager;
+        public PaymentController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet("PaymentInt/{Id}")]
+        public async Task<IActionResult> ProcessPayment(int Id)
+        {
+            var result = await _serviceManager.paymentService.CreateOrUpdatePaymentIntentRideCoinsAsync(Id);
+            return Ok(result);
+        }
     }
 }
