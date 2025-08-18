5712a6f9ffea15230672ee386175ec815349d476
basma zain <basmazain102000@gmail.com>
2025-08-18 16:30:58 +0300

    resolve missing data

--- Presentation/Controllers/PaymentController.cs
+++ Presentation/Controllers/PaymentController.cs
@@ -7,6 +7,7 @@ using Domain.Entities.credit;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
+using SharedData.DTOs.ChatDTOs;
 using SharedData.DTOs.Payment;
 using SharedData.Wrapper;
 
@@ -35,5 +36,27 @@ namespace Presentation.Controllers
         //    var Id = await _serviceManager.paymentService.CreateChargeEntityAsync(coins);
         //    return Ok(Id);
         //}
+
+        [HttpPost("CreateTransaction/{coinChargeId}")]
+        public async Task<IActionResult> CreateTransaction(int coinChargeId)
+        {
+            if(coinChargeId <= 0)
+            {
+                return BadRequest( $"{ coinChargeId} is not a valid coin charge ID.");
+
+            }
+            await _serviceManager.paymentService.CreateTransaction(coinChargeId);
+            return Ok(ApiResponse<CoinTopUp>.SuccessResponse(null, "transaction created successfully"));
+        }
+        [HttpGet("GetPersonalCoins")]
+        public async Task<IActionResult> GetPersonalCoins()
+        {
+            var coins = await _serviceManager.paymentService.GetPersonalCoins();
+            if (coins < 0)
+            {
+                return NotFound(ApiResponse<int>.FailResponse("No coins found"));
+            }
+            return Ok(ApiResponse<int>.SuccessResponse(coins, "Coins retrieved successfully"));
+        }
     }
 }
