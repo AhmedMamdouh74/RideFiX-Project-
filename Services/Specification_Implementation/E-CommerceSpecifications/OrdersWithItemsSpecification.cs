2e26e25a90bd17c20de4f2d3157af6e4547786bb
abdo-coco <https://github.com/abdo-coco>
2025-08-18 23:02:34 +0300

    orders

--- Services/Specification_Implementation/E-CommerceSpecifications/OrdersWithItemsSpecification.cs
+++ Services/Specification_Implementation/E-CommerceSpecifications/OrdersWithItemsSpecification.cs
@@ -13,13 +13,14 @@ namespace Service.Specification_Implementation.E_CommerceSpecifications
         public OrdersWithItemsSpecification() : base(x => 1 == 1)
         {
             AddInclude(x => x.orderItems);
-            AddInclude(x => x.orderItems.Select(i => i.Product));
+            AddInclude("orderItems.Product");
+
         }
 
         public OrdersWithItemsSpecification(int orderId) : base(o => o.Id == orderId)
         {
             AddInclude(x => x.orderItems);
-            AddInclude(x => x.orderItems.Select(i => i.Product));
+            AddInclude("orderItems.Product");
         }
     }
 }
